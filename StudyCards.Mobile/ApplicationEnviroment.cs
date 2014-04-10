2de92ebeb02f3b83287506438573e9cf10c39d87
jhenier13 <jhe.nier13@gmail.com>
2014-04-10 18:08:42 -0400

    Delete Desk (2nd part)
    Some new files and other deleted

---
+++ StudyCards.Mobile/ApplicationEnviroment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace StudyCards.Mobile
+{
+    public static class ApplicationEnviroment
+    {
+        private static string TEMPLATES = "Templates";
+        private static string IMAGES = "Images";
+        private static string BACKGROUNDS = "Backgrounds";
+        private static string CUSTOM = "Custom";
+        private static string DEFAULT = "Default";
+        public static readonly string TEMPLATES_DIRECTORY;
+        public static readonly string DEFAULT_TEMPLATES_DIRECTORY;
+        public static readonly string CUSTOM_TEMPLATES_DIRECTORY;
+        public static readonly string IMAGES_DIRECTORY;
+        public static readonly string BACKGROUNDS_DIRECTORY;
+        public static readonly string DEFAULT_BACKGROUNDS_DIRECTORY;
+        public static readonly string CUSTOM_BACKGROUNDS_DIRECTORY;
+
+        static ApplicationEnviroment()
+        {
+            TEMPLATES_DIRECTORY = Path.Combine(EnviromentDirectories.IOS_LIBRARY_DIRECTORY, TEMPLATES);
+            DEFAULT_TEMPLATES_DIRECTORY = Path.Combine(TEMPLATES_DIRECTORY, DEFAULT);
+            CUSTOM_TEMPLATES_DIRECTORY = Path.Combine(TEMPLATES_DIRECTORY, CUSTOM);
+            IMAGES = Path.Combine(EnviromentDirectories.IOS_LIBRARY_DIRECTORY, IMAGES);
+            BACKGROUNDS_DIRECTORY = Path.Combine(EnviromentDirectories.IOS_LIBRARY_DIRECTORY, BACKGROUNDS);
+            DEFAULT_BACKGROUNDS_DIRECTORY = Path.Combine(BACKGROUNDS_DIRECTORY, DEFAULT);
+            CUSTOM_BACKGROUNDS_DIRECTORY = Path.Combine(BACKGROUNDS_DIRECTORY, CUSTOM);
+        }
+    }
+}
+
---
+++ StudyCards.Mobile/Persistence/DataBaseUpdater.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Mono.Data.Sqlite;
+
+namespace StudyCards.Mobile.Persistence
+{
+    public static class DataBaseUpdater
+    {
+        //LastOpenedDate    Format:YYYYMMDDHHMM
+        private static string DESK_SCHEMA = @"CREATE TABLE Desks (
+                                                 DeskID             INTEGER     PRIMARY KEY     NOT NULL,
+                                                 Name               VARCHAR,
+                                                 CardFrontTemplate  VARCHAR,
+                                                 CardBackTemplate   VARCHAR,
+                                                 Background         VARCHAR
+                                                 )";
+//        private static string SHAKEITEM_SCHEMA = @"CREATE TABLE ShakeItems (
+//                                                 ItemID             INTEGER     PRIMARY KEY     NOT NULL,
+//                                                 ListID             INTEGER,
+//                                                 Name               VARCHAR,
+//                                                 ItemIndex              INTEGER,
+//                                                 IsLocked           INTEGER,
+//                                                 IsMarked           INTEGER
+//                                                 )";
+        private static string APPLICATION_SCHEMA = @"CREATE TABLE Application (
+                                                   Version      VARCHAR
+                                                   )";
+
+        public static void TryCreateDataBase()
+        {
+            bool databaseExists = File.Exists(DBEnviroment.DATABASE_FILE_PATH);
+
+            if (!databaseExists)
+            {
+                Directory.CreateDirectory(DBEnviroment.DATABASE_DIRECTORY_PATH);
+                SqliteConnection.CreateFile(DBEnviroment.DATABASE_FILE_PATH);
+                CreateDataBaseSchema();
+            }
+        }
+
+        private static void CreateDataBaseSchema()
+        {
+            SQLiteLinker.ExecuteQuery(DESK_SCHEMA);
+            SQLiteLinker.ExecuteQuery(APPLICATION_SCHEMA);
+
+            string insertVersionQuery = string.Format("INSERT INTO Application(Version) VALUES (\'{0}\')", "1.0.0");
+            SQLiteLinker.ExecuteQuery(insertVersionQuery);
+        }
+    }
+}
+
